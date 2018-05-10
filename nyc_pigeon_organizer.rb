6ee93c273ea0a27c6ca3bf7844b2dbf6efa1de7e
Anthony Lam <al3101@nyu.edu>
2018-05-10 00:47:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |trait, value|
     value.each do |key, bird|
+      pigeon_list[bird] = {}
+      pigeon_list[bird][trait] = []
       
 end
